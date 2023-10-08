3a138d7b199774f799b0e8236f1e40180f8d4723
Divay <divay@Divays-MacBook-Air.local>
2023-10-08 16:37:53 +0530

    Hello bhaii git mac mai swagat haii

---
+++ A_Jellyfish_and_Game.cpp
@@ -0,0 +1,43 @@
+/*
+|| ॐ त्र्यम्बकं यजामहे सुगन्धिं पुष्टिवर्धनम् उर्वारुकमिव बन्धनान्मृ त्योर्मुक्षीय मामृतात् ||
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define md 1000000007
+#define vll vector<ll>
+#define v2(dt, name, n, m, ini) vector<vector<dt>> name(n, vector<dt>(m, ini))
+#define pll pair<ll, ll>
+#define pb push_back
+#define ub upper_bound
+#define lb lower_bound
+#define endl "\n"
+#define fori(i, xx, yy) for (int i = xx; i < yy; i++)
+#define forl(i, xx, yy) for (ll i = xx; i < yy; i++)
+#define forir(i, xx, yy) for (int i = xx; i >= yy; i--)
+#define forlr(i, xx, yy) for (ll j = xx; j >= yy; j--)
+#define all(i) i.begin(), i.end()
+#define yn(i) if (i) cout << "YES\n"; else cout << "NO\n";
+#define debug(i) cout << #i << " = " << i << endl
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t = 1;
+    cin >> t;
+    for (int ii = 1; ii <= t; ii++)
+    {
+        int n;
+        cin >> n;
+        vll a(n);
+        fori(i, 0, n)
+        {
+            cin >> a[i];
+        }
+    }
+    return 0;
+}
+/*
+|| ॐ त्र्यम्बकं यजामहे सुगन्धिं पुष्टिवर्धनम् उर्वारुकमिव बन्धनान्मृ त्योर्मुक्षीय मामृतात् ||
+*/
